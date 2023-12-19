436a23aa8194898117dee9d38beb7b8b9ada176a
hristo.genev <hristo.genev@gmail.com>
2023-12-19 09:12:19 +0200

    fixed issue with empty line

--- logformatter.py
+++ logformatter.py
@@ -1,12 +1,14 @@
 def fix_reporting_url(line):
+    if not line:
+        return
     if "reporting.perfectomobile" in line:
-        if not line:
-            return
         line = line \
             .replace("reporting.perfectomobile", "app.perfectomobile") \
             .replace("[", "%5B") \
             .replace("]", "%5D")
         return line
+
+
 # #\x1b[38;5;115m \x1b[0m
 # \x1b[38;5;240m \x1b[0m
 #
@@ -30,4 +32,3 @@ def fix_coloring(line):
         line = line.replace("Your management id is: ", "")
         line = "Your management id is: \x1b[38;5;115m%s\x1b[0m" % line
     return line
-
